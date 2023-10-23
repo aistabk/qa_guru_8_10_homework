c3e6d53423a4c29cb4686da1631ef8a32ee6d648
Eli <129672457+aistabk@users.noreply.github.com>
2023-10-23 22:14:46 +0300

    Update image.py

--- data/image.py
+++ data/image.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-import os
 
 
 def path(file_name):
-    base_path = Path(__file__).resolve().parent.parent  # Получаем путь до корневой директории проекта
-    tests_folder_path = base_path / 'tests'  # Добавляем папку "tests" к пути
-    return str(tests_folder_path.joinpath(file_name))
+    base_path = Path(__file__).resolve().parent.parent
+    tests_folder_path = base_path / 'tests'
+    absolute_file_path = tests_folder_path / file_name
+    return str(absolute_file_path.resolve())
